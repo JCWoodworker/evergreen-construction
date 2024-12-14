080f7b7f7f0eed771fd9c3a74d51a3f21076a786
JCWoodworker <jc@rilocalwoodworks.com>
2024-12-14 16:50:12 -0500

    changed headings

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
 				alt="Evergreen Construction Logo"
 				style={{ width: "200px", height: "200px", borderRadius: "10px" }}
 			/>
-			<h1>Future Home of Evergreen Construction, LLC</h1>
+			<h2>Future Home of</h2>
+			<h1>Evergreen Construction, LLC</h1>
 		</>
 	)
 }
