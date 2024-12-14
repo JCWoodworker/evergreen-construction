df92839d833a15c4d47d6b914e8fc25e71efd33c
JCWoodworker <jc@rilocalwoodworks.com>
2024-12-14 16:47:01 -0500

    added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css"
 
 function App() {
-
 	return (
 		<>
+			<img
+				src="/evergreen-construction-logo.svg"
+				alt="Evergreen Construction Logo"
+			/>
 			<h1>Future Home of Evergreen Construction, LLC</h1>
 		</>
 	)
