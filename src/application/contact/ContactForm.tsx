db7d7e688b6cd1515f28e98ea2e7db1826d2a046
JCWoodworker <jc@rilocalwoodworks.com>
2024-12-15 08:51:54 -0500

    updated contact form

--- src/application/contact/ContactForm.tsx
+++ src/application/contact/ContactForm.tsx
@@ -1,40 +1,51 @@
-import { Button } from '@mui/material';
-import { useForm, ValidationError } from '@formspree/react';
+import { Button, TextField, Typography } from "@mui/material"
+import {
+	useForm,
+	ValidationError,
+} from "@formspree/react"
 
 function Contact() {
-  const [state, handleSubmit] = useForm("xwpkderv");
-  if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <Button type="submit" disabled={state.submitting}>
-        Submit
-      </Button>
-    </form>
-  );
+	const [state, handleSubmit] = useForm("xwpkderv")
+	if (state.succeeded) {
+		alert("Thank you for contacting us!")
+		window.location.href = "/";
+	}
+	return (
+		<form onSubmit={handleSubmit}>
+			<Typography variant="h4" gutterBottom>
+				Contact Us
+			</Typography>
+			<TextField
+				id="email"
+				label="Email Address"
+				type="email"
+				name="email"
+				fullWidth
+				margin="normal"
+				variant="outlined"
+			/>
+			<ValidationError prefix="Email" field="email" errors={state.errors} />
+			<TextField
+				id="message"
+				label="Message"
+				name="message"
+				multiline
+				rows={4}
+				fullWidth
+				margin="normal"
+				variant="outlined"
+			/>
+			<ValidationError prefix="Message" field="message" errors={state.errors} />
+			<Button
+				type="submit"
+				disabled={state.submitting}
+				variant="contained"
+				color="primary"
+			>
+				Submit
+			</Button>
+		</form>
+	)
 }
 
 export default Contact
