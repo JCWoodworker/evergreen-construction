353af7a5686408dd5fb5f9a36280e191478022b5
JCWoodworker <jc@rilocalwoodworks.com>
2024-12-14 23:51:08 -0500

    added commented out routess, to be coded later

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,15 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/contact" element={<ContactForm />} />
+
+				{/* TODO: Add these components */}
+				{/* <Route path="/about" element={<About />} />
+				<Route path="/services" element={<Services />} />
+				<Route path="/portfolio" element={<Portfolio />} />
+				<Route path="/blog" element={<Blog />} />
+				<Route path="/blog/:id" element={<BlogPost />} />
+				<Route path="/customer-portal" element={<CustomerPortal />} /> */}
+
 			</Routes>
 		</>
 	)
