e0829a5070cf1563218753d456916f15b82a1146
JCWoodworker <jc@rilocalwoodworks.com>
2024-12-14 23:14:05 -0500

    commented out contact page for now

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { Routes } from "react-router-dom"
 import "./App.css"
 
 import Home from "./application/home/Home"
-import ContactForm from "./application/contact/ContactForm"
+// import ContactForm from "./application/contact/ContactForm"
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/contact" element={<ContactForm />} />
+				{/* <Route path="/contact" element={<ContactForm />} /> */}
 			</Routes>
 		</>
 	)
